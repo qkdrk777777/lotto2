a1e06d32c53a281efaee2242d6e644e1e79d0b4c
qkdrk777777 <qkdrk777777@naver.com>
2018-03-18 01:53:21 +0900

    update

--- lotto2/app.R
+++ lotto2/app.R
@@ -1,50 +1,25 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+if(!require(devtools))install.packages('devtools') else library(devtools)
+library(devtools)
+if(!require('DUcj'))devtools::install_github('qkdrk777777/DUcj',force=T)
+library(DUcj)
+if(!require(lotto2))devtools::install_github('qkdrk777777/lotto2',force=T)
+library(lotto2)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-
-   # Sidebar with a slider input for number of bins
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+ui<-fluidPage(title='lotto sample',
+              numericInput('drop','제거할 숫자:',0,min=1,max=45),
+              numericInput('keep','유지할 숫자:',0,min=1,max=45),
+              numericInput('hold','무조건 나오게할 숫자:',0,min=1,max=45),
+              numericInput('n','뽑을 숫자:',10,min=1,max=100000),
+              numericInput('header','보여줄 숫자(type2,typ3에 해당)의 개수',10,min=1,max=100),
+              dataTableOutput('type2')
 )
+server<-function(input, output,session){
+  output$type2<-renderDataTable({lotto2(drop=input$drop,keep=input$keep,
+                                        hold=input$hold,n=input$n,
+                                        header=input$header)})
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2]
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
 }
 
-# Run the application
-shinyApp(ui = ui, server = server)
 
+shinyApp(ui,server)
