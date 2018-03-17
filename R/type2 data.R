7cfdd316b1a0065c82b5886646252134dbf3a440
qkdrk777777 <qkdrk777777@naver.com>
2018-03-17 23:42:04 +0900

    update

--- R/type2 data.R
+++ R/type2 data.R
@@ -2,11 +2,11 @@
 #'
 #' @return lotto data2
 #' @examples
-#' data2<-data_update2()
+#' data_update2()
 #' round(cor(data2),3)
 #' @export
 data_update2<-function(data=data_update()){
-  lottonum<<-data
+
   data2<-matrix(0,ncol=45,nrow=nrow(data));colnames(data2)=1:45
   for(i in 1:nrow(data2))
   {for(j in 1:6)
@@ -14,6 +14,7 @@ data_update2<-function(data=data_update()){
     if(data[i,j]==k)data2[i,k]<-1
   }}
   rownames(data2)<-nrow(data2):1
-  return(data2)}
-#type2data<-data_update2()
+  lottonum2<<-data2
+    return(data2)}
+
 #devtools::use_data(type2data, internal = F,overwrite=T)
